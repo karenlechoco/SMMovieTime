394cf7e6c83e372f94c36c0ebfd0c24db95a339a
Naphtali Lahoylahoy <naplahoylahoy@gmail.com>
2013-03-08 17:10:51 +0800

    SignUp Validations

--- src/com/sm/movietime/CinemaSignup.java
+++ src/com/sm/movietime/CinemaSignup.java
@@ -1,34 +1,90 @@
 package com.sm.movietime;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CinemaSignup extends Activity {
 
 	Intent i;
+	int flag = 0;
+	String strNotif;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cinema_signup);
-        
+                       
         Button submit = (Button)findViewById(R.id.signup_submit);
         submit.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				i = getIntent();
-				i.setClass(getBaseContext(), PurchaseBreakdown.class);
-				startActivity(i);
-				
+				EditText firstname = (EditText) findViewById(R.id.editText1);
+		        String strFirstname = firstname.getText().toString();
+		        EditText lastname = (EditText) findViewById(R.id.editText2);
+		        String strLastname = lastname.getText().toString();
+		        EditText middlename = (EditText) findViewById(R.id.editText3);
+		        String strMiddlename = middlename.getText().toString();
+		        EditText city = (EditText) findViewById(R.id.editText5);
+		        String strCity = city.getText().toString();
+		        /*birthdate*/
+		        EditText email1 = (EditText) findViewById(R.id.editText10);
+		        String strEmail1 = email1.getText().toString();
+		        EditText email2 = (EditText) findViewById(R.id.editText11);
+		        String strEmail2 = email2.getText().toString();
+		        EditText password1 = (EditText) findViewById(R.id.editText12);
+		        String strPassword1 = password1.getText().toString();
+		        EditText password2 = (EditText) findViewById(R.id.editText13);
+		        String strPassword2 = password2.getText().toString();
+		                
+		        if (strFirstname.isEmpty() && strLastname.isEmpty() && strMiddlename.isEmpty() && strCity.isEmpty()) {
+		        	flag = 1;
+		        	strNotif = "Some required fields are empty";
+		        }
+		        else if(emailValidator(strEmail1) != true) {
+		        	flag = 1;
+		        	strNotif = "Invalid Email Address";
+		        }
+		        else if(!(strEmail1.equals(strEmail2))) {
+		        	flag = 1;
+		        	strNotif = "Email Addresses do not match";
+		        }
+		        else if(!(strPassword1.equals(strPassword2))) {
+			        flag = 1;
+			        strNotif = "Passwords do not match";
+		        }
+		        	
+		        if(flag == 0) {
+		        	i = getIntent();
+					i.setClass(getBaseContext(), PurchaseBreakdown.class);
+					startActivity(i);
+		        }
+		        else {
+		        	Toast.makeText(getBaseContext(), strNotif, Toast.LENGTH_LONG).show();
+		        	flag = 0;
+		        	strNotif = "";
+		        } 		        					
 			}
 		});
-        
-        
+                
+    }
+    
+    public boolean emailValidator(String email) {
+        Pattern pattern;
+        Matcher matcher;
+        final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 
     @Override
